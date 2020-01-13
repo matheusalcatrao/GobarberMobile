88f277da88632d09846daa37043143942a0b718c
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2020-01-12 22:13:38 -0300

    eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,13 @@ module.exports = {
     env: {
         browser: true,
         es6: true,
+        jest: true,
     },
-    extends: ['airbnb'],
+    extends: ['airbnb', 'prettier', 'prettier/react'],
     globals: {
         Atomics: 'readonly',
         SharedArrayBuffer: 'readonly',
+        __DEV__: true,
     },
     parserOptions: {
         ecmaFeatures: {
@@ -15,11 +17,19 @@ module.exports = {
         ecmaVersion: 2018,
         sourceType: 'module',
     },
-    plugins: ['react', 'prettier', 'react-hooks'],
+    plugins: ['react', 'prettier', 'react-hooks', 'jsx-a11y', 'import'],
     rules: {
         'prettier/prettier': 'error',
         'react/jsx-filename-extension': ['warn', { extensions: ['jsx', 'js'] }],
         'import/prefer-default-export': 'off',
+        'no-unused-vars': ['error', { argsIgnprePattern: '^_' }],
+        'react/jsx-one-expression-per-line': 'off',
+        'global-require': 'off',
+        'react-native/no-raw-text': 'off',
+        'no-param-reassign': 'off',
+        'no-underscore-dangle': 'off',
+        camelcase: 'off',
+        'no-console': ['error', { allow: ['tron'] }],
         'react-hooks/rules-of-hooks': 'error',
         'react-hooks/exhaustive-deps': 'warn',
     },
