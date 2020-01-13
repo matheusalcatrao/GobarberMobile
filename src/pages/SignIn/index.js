c12a5dde7968adcffb746425e8d28a81568d7543
Matheus Alcatrão <matheusalcatrao@hotmail.com>
2020-01-12 22:36:13 -0300

    Backgound Linear Grandient

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import { Text } from 'react-native';
-
+import Background from '~/components/Background';
 // import { Container } from './styles';
 
 export default function SignIn() {
-    return <Text>SingnIn</Text>;
+    return (
+        <Background>
+            <Text>SingnIn</Text>
+        </Background>
+    );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,11 @@
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+// import { createStackNavigator } from 'react-navigation-stack';
 
 import SiginIn from '~/pages/SignIn';
 import SiginUp from '~/pages/SignUp';
 
 const routes = createAppContainer(
-    createStackNavigator({
+    createSwitchNavigator({
         SiginIn,
         SiginUp,
     })
